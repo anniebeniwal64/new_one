e75bc6de2f93d5fefe40f80eebbfd76cf9b877b3
Anu Beniwal <53251257+anniebeniwal64@users.noreply.github.com>
2019-07-25 11:41:37 +0530

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,13 @@
+"""Reverse a string
+N = input()
+LSTR = []
+RSTR = []
+for i in N:
+    LSTR.append(i)
+for i in range(len(N)):
+    RSTR.append(LSTR.pop())
+NEW_STRING = "".join(RSTR)
+print("New string is {}".format(NEW_STRING))
+"""
+N = input()
+print(N[::-1])
